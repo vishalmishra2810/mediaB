891922653afd71aab78765f26e0f6ab06256b09e
vishal <vishal.m@snapmint.com>
2024-03-13 01:14:03 +0530

    changes

--- dist/server.js
+++ dist/server.js
@@ -3,11 +3,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// src/server.ts
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const routes_1 = __importDefault(require("./routes"));
 const app = (0, express_1.default)();
-const PORT = 3000;
+const PORT = 8080;
+// MongoDB connection URL
+const MONGODB_URI = "mongodb+srv://journalmotionlive:KpLg6VGk9DkPH7yh@jmotion.dslkyer.mongodb.net/?retryWrites=true&w=majority&appName=JMotion";
+// Define MongoDB connection options
+const mongooseOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+// Connect to MongoDB
+mongoose_1.default.connect(MONGODB_URI, mongooseOptions)
+    .then(() => {
+    console.log("MongoDB connected");
+})
+    .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1); // Exit process with failure
+});
 app.use(express_1.default.json());
 app.use("/", routes_1.default);
 app.get("/", (req, res) => {
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,28 @@
-// src/server.ts
 import express from "express";
+import mongoose, { ConnectOptions } from "mongoose";
 import Router from "./routes";
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
+
+// MongoDB connection URL
+const MONGODB_URI = "mongodb+srv://journalmotionlive:KpLg6VGk9DkPH7yh@jmotion.dslkyer.mongodb.net/?retryWrites=true&w=majority&appName=JMotion";
+
+// Define MongoDB connection options
+const mongooseOptions: any = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// Connect to MongoDB
+mongoose.connect(MONGODB_URI, mongooseOptions)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1); // Exit process with failure
+  });
 
 app.use(express.json());
 app.use("/", Router);
